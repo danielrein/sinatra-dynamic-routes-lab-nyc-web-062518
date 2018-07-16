f41c1b23d863e3cbee78b01e34b44eed6c5d709e
Daniel Ran <danielr1735@gmail.com>
2018-07-16 20:56:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,4 +25,8 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{word1} #{word2} #{word3} #{word4} #{word5}"
   end
+
+  get '/:operation/:number1/:number2' do
+    "#{word1} #{word2} #{word3} #{word4} #{word5}"
+  end
 end
