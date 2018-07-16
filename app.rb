4f6e8d2b41ff5b9d5f3e0f531a27696babff2b63
Daniel Ran <danielr1735@gmail.com>
2018-07-16 20:40:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    :number.to_i.times { "#{:phrase}" }
+    n = :number.to_i
+    n.times { "#{:phrase}" }
   end
 
   get '/square/:number' do
